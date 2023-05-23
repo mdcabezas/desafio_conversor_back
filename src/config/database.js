0ba32264dbe7a3931d0f7537f7bf1cec54249bd2
Manuel Cabezas <manuelcabezas@gmail.com>
2023-05-23 02:42:26 -0400

    fix: parameters deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,12 @@
 import pg from 'pg';
 const { Pool } = pg;
 export const pool = new Pool({
-    host: process.env.PG_HOST || "192.168.1.66",
-    user: process.env.PG_USER || "postgres",
-    password: process.env.PG_PASS || "miclavesecreta",
-    database: process.env.PG_DB || "convert",
-    port: process.env.PORT || 5432,
+    host: process.env.PGHOST || "192.168.1.66",
+    port: process.env.PGPORT || 5432,
+    user: process.env.PGUSER || "postgres",
+    password: process.env.PGPASSWORD || "miclavesecreta",
+    database: process.env.PGDATABASE || "convert",
+
     allowExitOnIdle: true
 })
 
