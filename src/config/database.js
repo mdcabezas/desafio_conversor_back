a5a5195420c5c758c10e7785a7d36617f0133281
Manuel Cabezas <manuelcabezas@gmail.com>
2023-05-23 02:29:39 -0400

    feat: add environment variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,11 @@
 import pg from 'pg';
 const { Pool } = pg;
 export const pool = new Pool({
-    host: "192.168.1.66",
-    user: "postgres",
-    password: "miclavesecreta",
-    database: "convert",
+    host: process.env.PG_HOST || "192.168.1.66",
+    user: process.env.PG_USER || "postgres",
+    password: process.env.PG_PASS || "miclavesecreta",
+    database: process.env.PG_DB || "convert",
+    port: process.env.PORT || 5432,
     allowExitOnIdle: true
 })
 
